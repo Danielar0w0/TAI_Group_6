1c52628a7e673d7a4c25bfd2b74b80296eb6b292
Hugo1307 <hugomiguel_2001_@sapo.pt>
2023-03-26 19:57:54 +0100

    Fixed multiple bugs in Growing Copy Model builder.

--- src/model_builder/implementation/GrowingWindowModelBuilder.cpp
+++ src/model_builder/implementation/GrowingWindowModelBuilder.cpp
@@ -16,12 +16,14 @@ void GrowingWindowModelBuilder::buildModel(double alpha, double threshold) {
         // Get Window sequence as string
         std::string sequenceAsString = convertCharArrayToString(fileReader.getWindowContent(), fileReader.getWindowSize());
 
+        // std::cout << "Reading Window " << sequenceAsString << " on position " << fileReader.getCurrentPosition() << std::endl;
+
         // We have seen this sequence in the past
-        if (pastSequencesPositions.count(sequenceAsString) > 0) {
+        if (pastSequencesPositions.count(sequenceAsString) > 0 && pastSequencesPositions[sequenceAsString][0] < fileReader.getCurrentPosition()-1) {
 
             int currentPointerIndex;
 
-            // If I haven't chosen a pointer for this sequence yet, chose the first pointer
+            // If I haven't chose a pointer for this sequence yet, chose the first pointer
             if (currentPointerIndexForSequence.count(sequenceAsString) <= 0) {
                 currentPointerIndex = 0;
                 currentPointerIndexForSequence.insert(std::make_pair(sequenceAsString, currentPointerIndex));
@@ -29,10 +31,12 @@ void GrowingWindowModelBuilder::buildModel(double alpha, double threshold) {
                 currentPointerIndex = currentPointerIndexForSequence[sequenceAsString];
             }
 
-            // If we saw this sequence before, just add this new position
-            pastSequencesPositions[sequenceAsString].push_back(fileReader.getCurrentPosition());
+            // If we saw this sequence before, just add this new position if it doesn't exists already
+            if (std::find(pastSequencesPositions[sequenceAsString].begin(),
+                          pastSequencesPositions[sequenceAsString].end(),fileReader.getCurrentPosition()) == pastSequencesPositions[sequenceAsString].end())
+                pastSequencesPositions[sequenceAsString].push_back(fileReader.getCurrentPosition());
 
-            int pastSequencePosition = pastSequencesPositions[sequenceAsString].rbegin()[currentPointerIndex];
+            int pastSequencePosition = pastSequencesPositions[sequenceAsString][currentPointerIndex];
 
             // The probability P (probability of the next character being equal to the one I'm seeing now in the
             // current copy model)
@@ -41,13 +45,17 @@ void GrowingWindowModelBuilder::buildModel(double alpha, double threshold) {
             // Hits and misses info for the current copy model
             HitsMissesInfo hitsMissesInfo;
 
+            int count = 0;
+            int consecutiveMisses = 0;
+
             // Expand window until we reach the end of the file of the probability reaches a certain threshold
             do {
 
-                char predictedChar = fileReader.getCache()->operator[](pastSequencePosition);
+                // int pastSequenceOffset = (int)fileReader.getCurrentSequence()->size() - (fileReader.getWindowSize()-1);
+                char predictedChar = fileReader.getCache()->operator[](pastSequencePosition + count++);
                 char nextCharacterInSequence = fileReader.getNextCharacterInSequence();
 
-                // Calculate the probability P
+                // Calculate the probability P (a.k.a. probability of Hit)
                 probabilityOfCorrectPrediction = calculateHitProbability(hitsMissesInfo.getHits(),
                                                                          hitsMissesInfo.getMisses(), alpha);
 
@@ -57,14 +65,14 @@ void GrowingWindowModelBuilder::buildModel(double alpha, double threshold) {
                 std::string lastWindowAsString = convertCharArrayToString(fileReader.getLastWindowContent(),
                                                                           fileReader.getWindowSize());
 
-                // If we saw this sequence before, just add this new position
-                pastSequencesPositions[lastWindowAsString].push_back(fileReader.getCurrentPosition());
+                // If we saw this sequence before, just add this new position if it doesn't exists already
+                if (std::find(pastSequencesPositions[lastWindowAsString].begin(),
+                              pastSequencesPositions[lastWindowAsString].end(),fileReader.getCurrentPosition()+1) == pastSequencesPositions[lastWindowAsString].end()) {
+                    pastSequencesPositions[lastWindowAsString].push_back(fileReader.getCurrentPosition()+1);
+                }
 
                 // Value to distribute for other characters
                 double complementaryProbability = 1-probabilityOfCorrectPrediction;
-
-                // Calculate the total amount of information
-                // totalAmountOfInformation += -std::log2(probabilityOfCorrectPrediction);
 
                 // If it turns out the next character in the copy model to be equal to the next character in the
                 // sequence then we have a hit.
@@ -72,13 +80,49 @@ void GrowingWindowModelBuilder::buildModel(double alpha, double threshold) {
 
                     hitsMissesInfo.incrementHits();
 
+                    std::vector<char> currentSequenceVector(fileReader.getCurrentSequence()->size());
+                    std::copy(fileReader.getCurrentSequence()->begin(), fileReader.getCurrentSequence()->end(), currentSequenceVector.begin());
+                    std::string currentSequence = convertCharVectorToString(currentSequenceVector);
+
+                    /*std::cout << std::endl;
+                    std::cout << "Hit on Character " << nextCharacterInSequence << std::endl;
+                    std::cout << "Past Sequence in " << pastSequencePosition + count << std::endl;
+                    std::cout << "Sequence is: " << currentSequence << ". Last window: " << lastWindowAsString << std::endl;
+                    std::cout << "Probability is " << probabilityOfCorrectPrediction << std::endl;
+                    std::cout << "Adding probability " << probabilityOfCorrectPrediction << std::endl;
+                    std::cout << std::endl;*/
+
+                    consecutiveMisses = 0;
+
                     // The total information is the sum of the information of each character at that point taking into
                     // account the probability of the character being correct
                     totalAmountOfInformation += -std::log2(probabilityOfCorrectPrediction);
 
+                    // std::cout << "Hit" << std::endl;
+
                 } else { // Otherwise, next character in sequence != of character in copy model, we have a miss
 
                     hitsMissesInfo.incrementMisses();
+
+                    std::vector<char> currentSequenceVector(fileReader.getCurrentSequence()->size());
+                    std::copy(fileReader.getCurrentSequence()->begin(), fileReader.getCurrentSequence()->end(), currentSequenceVector.begin());
+                    std::string currentSequence = convertCharVectorToString(currentSequenceVector);
+
+                    double prob = getProbabilityDistributionForCharacter(nextCharacterInSequence,
+                                                                         complementaryProbability);
+
+                    consecutiveMisses += 1;
+                    // std::cout << "Miss" << std::endl;
+
+                    /*std::cout << std::endl;
+                    std::cout << "Miss on Character " << nextCharacterInSequence << ". Expecting " << predictedChar << std::endl;
+                    std::cout << "Sequence is: " << currentSequence << ". Last window: " << lastWindowAsString << std::endl;
+                    std::cout << "Probability is " << probabilityOfCorrectPrediction << std::endl;
+                    std::cout << "Adding probability " << prob << std::endl;
+                    std::cout << "Past Sequence in " << pastSequencePosition + count << std::endl;
+
+                    std::cout << std::endl;
+                    std::cout << std::endl;*/
 
                     // The total information is the sum of the information of each character at that point taking into
                     // account the probability of the character being correct
@@ -87,7 +131,10 @@ void GrowingWindowModelBuilder::buildModel(double alpha, double threshold) {
 
                 }
 
-            } while (fileReader.nextCharacter());
+                std::printf("Progress (Growing Window Model): %d%%\r", getProgress());
+                std::fflush(stdout);
+
+            } while (probabilityOfCorrectPrediction >= threshold && fileReader.nextCharacter());
 
             // When we leave the loop we are stopping the copy model!
             std::string correspondingWindow = convertCharVectorToString(*fileReader.getCurrentSequence()).substr(0, fileReader.getWindowSize());
@@ -114,6 +161,9 @@ void GrowingWindowModelBuilder::buildModel(double alpha, double threshold) {
                 // currentPointerIndexForSequence[sequenceAsString] = (int) dist(rng);
                 currentPointerIndexForSequence[sequenceAsString] += 1;
 
+                // Read next character to shift next window
+                fileReader.nextCharacter();
+
             }
 
         } else {
@@ -127,10 +177,10 @@ void GrowingWindowModelBuilder::buildModel(double alpha, double threshold) {
 
         }
 
-        std::printf("Progress (Growing Window Model): %d%%\r", getProgress());
-        std::fflush(stdout);
-
     }
+
+    // Cache is correct
+    // std::cout << "Cache: " << convertCharVectorToString(*fileReader.getCache()) << std::endl;
 
     fileReader.closeFile();
 
