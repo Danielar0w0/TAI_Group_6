96c7cc67b656a701d0b836ca6cc4c588d8e59679
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2023-03-28 21:58:41 +0100

    Added interactive mode to Probabilistic Generator

--- src/generator/implementation/ProbabilisticGenerator.cpp
+++ src/generator/implementation/ProbabilisticGenerator.cpp
@@ -3,7 +3,6 @@
 #include <random>
 #include "ProbabilisticGenerator.h"
 
-
 ProbabilisticGenerator::ProbabilisticGenerator(ProbabilisticModelSerializer probabilisticModelSerializer)
         : probabilisticModelSerializer(std::move(probabilisticModelSerializer)) { }
 
@@ -19,12 +18,8 @@ void ProbabilisticGenerator::generateTextInteractively(int generationSize) {
 
         if (input == "exit") break;
 
-        std::string generatedText = generateText(generationSize, input, model, this->useOptimization);
-
-        if (generatedText.empty()) continue;
-
         std::cout << "[-] (" << input << ")";
-        std::cout << generatedText << std::endl;
+        generateText(generationSize, input, model, true);
 
     }
 
@@ -41,18 +36,24 @@ void ProbabilisticGenerator::generateTextOnce(int generationSize) {
 
     if (input == "exit") return;
 
-    std::string generatedText = generateText(generationSize, input, model, this->useOptimization);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    std::string generatedText = generateText(generationSize, input, model, false);
 
     if (generatedText.empty()) return;
 
     std::cout << "[-] (" << input << ")";
     std::cout << generatedText << std::endl;
 
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    logger.debug("Time taken to generate text: " + std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()) + "ms");
+
 }
 
 std::string ProbabilisticGenerator::generateText(int generationSize, const std::string &initialText,
                                                  const std::map<std::string, std::map<char, double>> &model,
-                                                 bool useFuturePredictionAlgorithm) {
+                                                 bool realTime) {
 
     if (initialText.size() < getModelWindowSize()) {
         std::cout << "[!] Please enter a sequence with at least " << getModelWindowSize() << " characters." << std::endl;
@@ -74,84 +75,36 @@ std::string ProbabilisticGenerator::generateText(int generationSize, const std::
         if (model.count(currentWindow) <= 0)
             break;
 
-        char nextChar = generateNextCharacter(generatedText, useFuturePredictionAlgorithm);
+        char nextChar = generateNextCharacter(generatedText);
 
-        currentWindow += nextChar;
         generatedText += nextChar;
-        currentWindow = currentWindow.substr(1, currentWindow.length() - 1);
 
+        if (realTime) {
+            std::cout << nextChar;
+            std::fflush(stdout);
+        }
+
+
+        currentWindow = generatedText.substr(generatedText.size()-getModelWindowSize(), generatedText.size());
+
+    }
+
+    if (realTime) {
+        std::cout << std::endl;
+        std::fflush(stdout);
     }
 
     return generatedText.substr(getModelWindowSize(), generatedText.length());
 
 }
 
-char ProbabilisticGenerator::generateNextCharacter(const std::string& generatedText, bool useFuturePredictionAlgorithm) {
+char ProbabilisticGenerator::generateNextCharacter(const std::string& generatedText) {
 
     std::string currentWindow = generatedText.substr(generatedText.size()-getModelWindowSize(), generatedText.size());
     std::map<std::string, std::map<char, double>> model = probabilisticModelSerializer.getModel();
     std::map<char, double> charactersProbabilityDistributionMap = model[currentWindow];
 
-    if (useFuturePredictionAlgorithm) {
-
-        int maximumThreshold = 100;
-        int triesCounter = 0;
-        std::string nextWindow;
-        char nextCharacterPrediction;
-        std::vector<char> notSafeCharacters;
-
-        while (true) {
-
-            char randomPrediction;
-
-            if (notSafeCharacters.size() >= charactersProbabilityDistributionMap.size()/5) {
-
-                for (const auto& [futureCharacter, _] : charactersProbabilityDistributionMap) {
-                    if (std::find(notSafeCharacters.begin(), notSafeCharacters.end(), futureCharacter) == notSafeCharacters.end()) {
-                        randomPrediction = futureCharacter;
-                        break;
-                    }
-                }
-
-            } else {
-                randomPrediction = getCharacterUsingUniformDist(charactersProbabilityDistributionMap);
-            }
-
-            currentWindow = currentWindow.substr(1, currentWindow.size()-1) + randomPrediction;
-
-            // If the window + the character I generated exists in the model
-            if (model.count(currentWindow) > 0) {
-
-                if (isCharacterSafeChoice(currentWindow, charactersProbabilityDistributionMap, 0, this->optimizationAggressiveness)) {
-                    nextCharacterPrediction = randomPrediction;
-                    notSafeCharacters.clear();
-                    std::cout << "Optimizing Output. Optimizing " << generatedText.size() << "th character." << "\r";
-                    std::fflush(stdout);
-                    break;
-                } else {
-                    notSafeCharacters.push_back(randomPrediction);
-                }
-
-                continue;
-
-            } else { // If the window + the character does not exist
-
-                if (++triesCounter >= maximumThreshold) {
-                    nextCharacterPrediction = randomPrediction;
-                    break;
-                }
-
-                continue;
-
-            }
-
-        }
-
-        return nextCharacterPrediction;
-
-    } else {
-        return getCharacterUsingUniformDist(charactersProbabilityDistributionMap);
-    }
+    return getCharacterUsingUniformDist(charactersProbabilityDistributionMap);
 
 }
 
@@ -174,41 +127,6 @@ char ProbabilisticGenerator::getCharacterUsingUniformDist(const std::map<char, d
 
 }
 
-bool ProbabilisticGenerator::isCharacterSafeChoice(const std::string& currentWindow,  const std::map<char, double>& charactersProbabilityDistribution,
-                                                   int currentIteration, int maxIterations) {
-    int totalCorrectMoves = 0;
-    std::string tempWindow;
-
-    if (currentIteration >= maxIterations) {
-        logger.debug("Reached max iterations: " + std::to_string(maxIterations));
-        return true;
-    }
-
-    // Check if in the next move it will come to a point where I'm trapped
-    for (const auto& [futureCharacter, _] : charactersProbabilityDistribution) {
-
-        tempWindow = currentWindow.substr(1, currentWindow.size()-1) + futureCharacter;
-
-        if (probabilisticModelSerializer.getModel().count(tempWindow) > 0) {
-            totalCorrectMoves++;
-        }
-
-    }
-
-    if (totalCorrectMoves >= this->optimizationAggressiveness/100*charactersProbabilityDistribution.size())
-        return isCharacterSafeChoice(tempWindow, charactersProbabilityDistribution, currentIteration + 1,maxIterations);
-    return false;
-
-}
-
 int ProbabilisticGenerator::getModelWindowSize() {
     return (int) probabilisticModelSerializer.getModel().begin()->first.size();
-}
-
-void ProbabilisticGenerator::setUseOptimization(bool useOptimizationParam) {
-    this->useOptimization = useOptimizationParam;
-}
-
-void ProbabilisticGenerator::setOptimizationAggressiveness(int optimizationAggressivenessParam) {
-    this->optimizationAggressiveness = optimizationAggressivenessParam;
 }
